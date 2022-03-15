e25a722577fb87140b7a28ff6de01d46d2e8d033
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2022-03-15 11:54:06 +0200

    before

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,5 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {}
-    public void test() {switch (1) {}}
+    public void test() {
+        System.out.println("hi");
+    }
 }
