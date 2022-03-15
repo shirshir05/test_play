5359500620cbb6866d0ff3867fe514d2d0358c37
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2022-03-15 11:54:26 +0200

    add switch (1) {}

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {}
     public void test() {
-        System.out.println("hi");
+        System.out.println("hi");switch (1) {}
     }
 }
