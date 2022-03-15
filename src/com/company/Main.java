7cf5f60ef5919ea41f1be836d35f4753fef72937
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2022-03-15 11:56:54 +0200

    before with complex

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {}
-    public void test() {
-        System.out.println("hi");switch (1) {}
+    public boolean test() {
+        try {
+            return true;
+        } catch (Throwable th) {
+            return false;
+        }
     }
 }
