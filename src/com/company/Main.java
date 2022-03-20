44e9215c25cf643efb601b6c319ecbf32045328e
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2022-03-20 16:01:59 +0200

    add class and init

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,6 @@
 package com.company;
 public class Main {
-    int x = 0;
+    int x = 0;public class test{public test(){}}
     //comment
     public static void main(String[] args) {Main m = new Main();}
 }
