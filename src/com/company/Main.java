ac13f85f5e9d781300dbda6c36e356f9f18a22f9
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2022-03-20 16:18:40 +0200

    before

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,6 @@ package com.company;
 public class Main {
     int x = 0;
     //comment
-    public static void main(String[] args) {Main m = new Main();}
+    public static void main(String[] args) {Main m = new Main();m.test();}
     public void test(){}
 }
