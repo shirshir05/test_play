d7f06d8b36c9c9f2b8b8db828031d4e8765fab21
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2022-03-20 16:48:12 +0200

    before

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,6 @@ package com.company;
 public class Main {
     int x = 0; int y =0;
     //comment
-    public static void main(String[] args) {Main m = new Main();switch (1){case 2:m.test();}}
-    public void test(){int b = 0;}
+    public static void main(String[] args) {Main m = new Main();m.test();}
+    public void test(){int b = 0; if(b==0){b=1;}System.out.println(b);}
 }
