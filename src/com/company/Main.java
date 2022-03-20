c1fa7c9150d358a19bf01c8aba5eb3b8b054aa9a
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2022-03-20 09:51:11 +0200

    before

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,5 @@
 package com.company;
 
 public class Main {
-
     public static void main(String[] args) {}
-    public boolean test() {
-        try {
-            return true;
-        } catch (Throwable th) {
-            return false;
-        }
-    }
 }
