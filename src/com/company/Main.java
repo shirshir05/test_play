4797cfce48f6c24b8b4c8f0b748485ad023c76b9
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2022-03-20 17:19:53 +0200

    add public attribute

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 public class Main {
     int x = 0; int y =0;
+    public int z=0;
     //comment
     public static void main(String[] args) {Main m = new Main();m.test();}
     public void test(){int b = 0;switch (1){case 2: try {throw new Exception();} catch (Exception e) {;}}}
