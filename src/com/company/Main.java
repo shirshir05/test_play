d1cc5e77f25b73075896f047f69215df4e2b3307
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2022-03-20 14:52:06 +0200

    before

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,5 @@ public class Main {
     int x = 0;
     //comment
     public static void main(String[] args) {Main m = new Main();m.test();}
-    public void test(){System.out.println("test1");System.out.println(x);switch(1){case 2: main((String[]) new Object());}}
+    public void test(){System.out.println("test1");System.out.println(x);}
 }
