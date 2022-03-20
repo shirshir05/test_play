e5062e0917cbe9d0d786a5e89fe9ca011e47a5f0
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2022-03-20 16:55:44 +0200

    AddIfFalse

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,5 +3,5 @@ public class Main {
     int x = 0; int y =0;
     //comment
     public static void main(String[] args) {Main m = new Main();m.test();}
-    public void test(){int b = 0;System.out.println(b);}
+    public void test(){int b = 0;if(false){}System.out.println(b);}
 }
