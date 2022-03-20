5c34065b000593d649ef0101735f897c12878a0c
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2022-03-20 16:15:02 +0200

    add init

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,6 @@
 package com.company;
 public class Main {
-    int x = 0;public class test{public test(){}}
+    int x = 0;public class test{public test(){} public test(int x){}}
     //comment
     public static void main(String[] args) {Main m = new Main();}
 }
