3ab65bd2928324544936886c7a0ef52514a6d4c0
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2022-03-20 11:05:03 +0200

    add comment

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class Main {
     int x = 0;
-    
-    public static void main(String[] args) {}
+
+    public static void main(String[] args) {}//comment
     public void test(){System.out.println("test1");}
 }
