ea52d3bf7ec7267b089c27c2da8d336ff9ddb37d
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2022-03-20 15:49:52 +0200

    assignAddWithSwitch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
     int x = 0;
     //comment
     public static void main(String[] args) {Main m = new Main();m.test();}
-    public void test(){int y =0;System.out.println("test1");System.out.println(x);}
+    public void test(){int y =0;System.out.println("test1");System.out.println(x);switch (1){case 2:y=1;}}
     public void test1(int x){x=0;x=1; x=0;}
 }
