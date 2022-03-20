8d781588df9f60dfc98719569991cab40bebba8d
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2022-03-20 16:20:56 +0200

    MethodCallSwitchCase

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,6 @@ package com.company;
 public class Main {
     int x = 0;
     //comment
-    public static void main(String[] args) {Main m = new Main();}
+    public static void main(String[] args) {Main m = new Main();switch (1){case 2:m.test();}}
     public void test(){}
 }
