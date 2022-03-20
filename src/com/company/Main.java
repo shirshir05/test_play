bb4395f54435747756c57c4a0dc0f8b6af55ce66
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2022-03-20 16:38:40 +0200

    add var

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,5 +3,5 @@ public class Main {
     int x = 0; int y =0;
     //comment
     public static void main(String[] args) {Main m = new Main();switch (1){case 2:m.test();}}
-    public void test(){}
+    public void test(){int b = 0;}
 }
