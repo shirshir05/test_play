196d3df93e6304115e9d56ef90951d6e2f5136e1
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2022-03-20 15:30:01 +0200

    AddReturn

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
     int x = 0;
     //comment
     public static void main(String[] args) {Main m = new Main();m.test();}
-    public void test(){System.out.println("test1");System.out.println(x);}
+    public void test(){System.out.println("test1");System.out.println(x);switch (1){case 2:return;}}
     public void test1(int x){}
 }
