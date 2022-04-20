abad592c36aac9eca53e42340b245e928c94d54a
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2022-04-20 17:44:11 +0300

    c

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,6 @@ public class Main {
     public static void main(String[] args) {
         Main m = new Main();m.test();
     }
-    public void test(){switch (2){case 1:main((String[]) new Object());}}
+    public void test(){ }
 
 }
