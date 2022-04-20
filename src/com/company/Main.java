099212ca3c6abfa07cda3be029fedc0047af32b0
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2022-04-20 13:47:09 +0300

    clean

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ public class Main {
     int x = 0; int y =0;
     public int z=0;
     //comment
-    public static void main(String[] args) {Main m = new Main();m.test();}
-    public void test(){while(true){break;}}
+    public static void main(String[] args) {
+        Main m = new Main();
+        m.test();
+    }
+    public void test(){ }
 }
