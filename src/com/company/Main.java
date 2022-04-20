208c577ecec689eb84293808eefc6a7854aa296c
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2022-04-20 17:58:09 +0300

    AddPublicAttribute

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,7 @@ package com.company;
 public class Main {
     int x = 0; int y =0;
     public int z=0;
-
+    public int c=0;
     public static void main(String[] args) {
         Main m = new Main();m.test();
     }
