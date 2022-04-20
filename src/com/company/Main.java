0033caf4afa17d22af9e27f684fa7df729d063ad
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2022-04-20 17:28:42 +0300

    clean

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,10 +2,10 @@ package com.company;
 public class Main {
     int x = 0; int y =0;
     public int z=0;
-    //comment
+
     public static void main(String[] args) {
         Main m = new Main();m.test();
     }
-    public void test(){if (1 == 2){main(null);}}
-    //comment
+    public void test(){ }
+
 }
