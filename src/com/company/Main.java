3f2ac2fe80df0ee75aac21a55f71c3884cc1c46f
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2022-04-20 18:05:26 +0300

    c

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Main m = new Main();m.test();
     }
-    public void test(){switch (1){case 2:while(true)continue;}}
+    public void test(){ }
 
 }
 
