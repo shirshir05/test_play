ac5bd91f6f2ed18c9b6b384af70b231678b28336
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2022-05-22 09:15:20 +0300

    after modification

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,12 @@ public class Main {
 
     public static void main(String[] args1) {
         System.out.println("Hello World!");
+        int x;
+        x = 5;
+        if (x % 2 == 0)
+            x = x + 1;
+        else
+            x += 2;
     }
 
 }
