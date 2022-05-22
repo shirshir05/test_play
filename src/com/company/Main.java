efff865f165bc63c88e903248e69863d7ae91ece
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2022-05-22 09:14:33 +0300

    after modification

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,9 @@
 package com.company;
 public class Main {
-    int x = 0; int y =0;
-    public int z=0;
 
     public static void main(String[] args1) {
-        Main m = new Main();m.test();
+        System.out.println("Hello World!");
     }
-    public void test(){int b=0; if(b==0){b=1;}else if(false);}
+
 }
 
