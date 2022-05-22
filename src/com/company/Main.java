e5f3ff30b851d3d9b7b8611e8b7a68bbbcd2d832
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2022-05-22 15:45:26 +0300

    after modification

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,11 @@ public class Main {
 
     public static void main(String[] args1) {
         System.out.println("Hello World!");
-        int x;
-        x = 5;
-        if (x % 2 == 0)
-            x = x + 1;
-        else
-            x += 2;
+        int x = 1 + 1;
+        x = x + 1 + 2;
+        while( x % 2 != 0)
+            x += 1;
+        x += 1;
     }
 
 }
