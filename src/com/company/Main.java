75c59122d7a956cae7826175835c553ed696a50d
shirshir05 <48806632+shirshir05@users.noreply.github.com>
2022-05-23 10:06:48 +0300

    after modification

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,6 +8,37 @@ public class Main {
         while( x % 2 != 0)
             x += 1;
         x += 1;
+
+
+
+        int x;
+        x = 5;
+        if (x % 2 == 0)
+            x = x + 1;
+        else
+            x += 2;
+
+
+        int x = 5;
+        x += x % 2 == 0 ? 1 : 2;
+
+
+
+        int x = 5;
+        switch (x % 2){
+            case 0:
+                x += 1;
+                break;
+            default:
+                x += 2;
+        }
+
+
+        int x = 1 + 1;
+        x = x + 1 + 2;
+        while(x % 2 != 0)
+            x += 1;
+        x += 1;
     }
 
 }
